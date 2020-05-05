068c0f2be72c56b78ce89b677f904cff53efd498
Fabio Romita <hotello.dev@gmail.com>
2020-05-05 16:52:16 +0200

    Basic layout only on desktop

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 
 import { Colors } from './constants';
 import MyPatientsScreen from './screens/MyPatientsScreen';
@@ -9,14 +9,41 @@ import NoteScreen from './screens/NoteScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <NoteScreen />
+      <View style={styles.header}></View>
+      <View style={styles.content}>
+        <View style={styles.left}>
+          <MyPatientsScreen />
+        </View>
+        <View style={styles.middle}>
+          <PatientScreen />
+        </View>
+        <View style={styles.right}>
+          <NoteScreen />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     backgroundColor: Colors.background,
-    flex: 1,
+    maxHeight: Dimensions.get('window').height - 50,
+    flexDirection: 'row',
+  },
+  header: {
+    backgroundColor: Colors.englishViolet,
+    height: 50,
+  },
+  left: {
+    backgroundColor: Colors.beige,
+    flex: 0.2,
+  },
+  middle: {
+    flex: 0.35,
+  },
+  right: {
+    backgroundColor: Colors.white,
+    flex: 0.45,
   },
 });
