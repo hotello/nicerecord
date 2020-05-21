94189fc50762554ab9ffe3c581921e0e46ab7bf2
Fabio Romita <hotello.dev@gmail.com>
2020-05-21 09:12:07 +0200

    Updates

---
+++ precache-manifest.a008ec08fee0dd9b0c0c7d70548ab077.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd39c477a1b00401e790",
+    "url": "/nicerecord/static/js/app.6ba27274.chunk.js"
+  },
+  {
+    "revision": "0082b59041de533e8b3e",
+    "url": "/nicerecord/static/js/runtime~app.daa96ac5.js"
+  },
+  {
+    "revision": "3acb9725d2dec273428a",
+    "url": "/nicerecord/static/js/2.d5bfb9c0.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/nicerecord/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/nicerecord/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/nicerecord/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/nicerecord/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nicerecord/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/nicerecord/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/nicerecord/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/nicerecord/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/nicerecord/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/nicerecord/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/nicerecord/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/nicerecord/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/nicerecord/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/nicerecord/expo-service-worker.js"
+  },
+  {
+    "revision": "3c899f8bc6ee56bdae8da913300fad5f",
+    "url": "/nicerecord/favicon-16.png"
+  },
+  {
+    "revision": "489cf7385477cba572437c12b3eea823",
+    "url": "/nicerecord/favicon-32.png"
+  },
+  {
+    "revision": "a5c36cae01a04b36f9061db8d97f4af5",
+    "url": "/nicerecord/favicon.ico"
+  },
+  {
+    "revision": "36ba6ebce9f05d461c709d3285d5c84d",
+    "url": "/nicerecord/index.html"
+  },
+  {
+    "revision": "b8ee120328a5bd0a34cd06c2b7590141",
+    "url": "/nicerecord/manifest.json"
+  },
+  {
+    "revision": "b89f71b8f59a32ad73fc25ba2aafab84",
+    "url": "/nicerecord/register-service-worker.js"
+  }
+];
