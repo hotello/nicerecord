35a001ff2eff9d653b441089e9c3ef384f4fa786
hotello <hotello.dev@gmail.com>
2020-06-07 19:07:31 +0200

    Remove rounded border in TextInputWindows

--- src/components/base/TextInput/TextInput.windows.js
+++ src/components/base/TextInput/TextInput.windows.js
@@ -37,7 +37,6 @@ export default React.forwardRef(function TextInputWindows(
 
 const styles = StyleSheet.create({
   input: {
-    borderRadius: Sizes.radius,
     marginBottom: Sizes.unit * 2,
     minHeight: Sizes.unit * 8,
   },
